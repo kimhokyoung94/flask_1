1f3b4191bd4e2e5126366bbdd02954faf26751f9
HoKoung Kim <ghrud24020@naver,com>
2020-07-23 17:49:23 +0900

    edit add ex

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask ,render_template , flash , redirect , url_for, session, request, logging
-#from flask_mysqldb import MySQL
+
 import pymysql
 from passlib.hash import pbkdf2_sha256
 from data import Articles
@@ -7,21 +7,12 @@ from data import Articles
 app = Flask(__name__)
 app.debug=True
 
-#config MySQL
-# app.config['MYSQL_HOST']='localhost'
-# app.config['MYSQL_USER']='root'
-# app.config['MYSQL_PASSWORD']='1234'
-# app.config['MYSQL_DB']='myflaskapp'
-# app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 db = pymysql.connect(host='localhost', 
                         port=3306, 
                         user='root', 
                         passwd='1234', 
                         db='myflaskapp')
-
-# cursor = db.cursor()
-
 
 
 #init mysql 
@@ -32,27 +23,6 @@ db = pymysql.connect(host='localhost',
 # users  = cur.fetchall()
 # print(users)
 # print(result)
-@app.route('/login',methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        id = request.form.get('email')
-        pw = request.form.get('password')
-        # print([id,pw])
-
-        sql = 'SELECT * FROM users WHERE email = %s'
-        cursor = db.cursor()
-        cursor.execute(sql,[id])
-        users = cursor.fetchone()
-        print(users[4])
-
-        if pbkdf2_sha256(pw,users[4]):
-            return redirect(url_for('home'))
-        else:
-            return redirect(url_for('login'))
-        
-        
-    else:
-        return render_template('login')
 
 @app.route('/')
 def index():
@@ -69,59 +39,140 @@ def about():
 @app.route('/register',methods=['GET' ,'POST'])
 def register():
     if request.method == 'POST':
-              
-        #data = request.body.get('author')
+
+        # data = request.body.get('author')
         name = request.form.get('name')
         email = request.form.get('email')
         password = pbkdf2_sha256.hash(request.form.get('password'))
         re_password = request.form.get('re_password')
         username = request.form.get('username')
-        #name = form.name.data
-        if (pbkdf2_sha256.verify(re_password,password)):
-            print(pbkdf2_sha256.verify(re_password,password))
-            # print(name, email, password, re_password, username)
+        # name = form.name.data
+        if(pbkdf2_sha256.verify(re_password,password )):
+            print(pbkdf2_sha256.verify(re_password,password ))
             cursor = db.cursor()
-
             sql = '''
-                INSERT INTO users (name, email, username, password)
-                VALUES (%s, %s, %s, %s);
-            '''
-            cursor.execute(sql, (name, email, username, password))
+                INSERT INTO users (name , email , username , password) 
+                VALUES (%s ,%s, %s, %s )
+             '''
+            cursor.execute(sql , (name,email,username,password ))
             db.commit()
             
-            
-            # cursor = db.cursor()
-            # cursor.execute('SELECT * FROM users')
-            # users = cursor.fetchall()
-            # db.close()
 
-            return "sucess"
-        else:
+            # cursor = db.cursor()
+            # cursor.execute('SELECT * FROM users;')
+            # users = cursor.fetchall()
             
-            return "Invaild password"
+            return redirect(url_for('login'))
+
+        else:
+            return "Invalid Password"
+
         db.close()
     else:
         return render_template('register.html')
 
-@app.route('/articles', methods=['GET','POST'])
-def articles():
-    print("Success")
-    # return "TEST"
-    articles = Articles()
-    print(len(articles))
-    return render_template('articles.html',articles=articles)
 
-@app.route('/test')
-def show_image():
-    return render_template('image.html')
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        id = request.form['email']
+        pw = request.form.get('password')
+        print([id])
+
+        sql='SELECT * FROM users WHERE email = %s'
+        cursor  = db.cursor()
+        cursor.execute(sql, [id])
+        users = cursor.fetchone()
+        print(users)
+
+        if users ==None:
+            return redirect(url_for('login'))
+        else:
+            if pbkdf2_sha256.verify(pw,users[4] ):
+                return redirect(url_for('articles'))
+            else:
+                return redirect(url_for('login'))
+        
+    else:
+        return render_template('login.html')
+
+@app.route('/articles')
+def articles():
+    # data = Articles()
+    # print(len(articles))
+    cursor = db.cursor()
+    sql='SELECT * FROM topic;'
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data)
+    return render_template('articles.html',articles=data)
+    # return "GET Success"
+
 
 @app.route('/article/<int:id>')
 def article(id):
-    print(id)
+    print(type(id))
     articles= Articles()[id-1]
-    print(articles)
-    return render_template('article.html',data =articles)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic WHERE id=%s;'
+    cursor.execute(sql,[id])
+    topic = cursor.fetchone()
+    print(topic)
+    # print(articles)
+    return render_template('article.html',data =topic)
     # return "Success"
+
+@app.route('/add_articles',methods=['GET','POST'])
+def add_articles():
+    if request.method == 'POST':
+        # print(request.form['title'])
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        cursor = db.cursor()
+        sql = '''
+            INSERT INTO topic (title, body , author)
+            VALUES (%s ,%s ,%s)
+        '''
+        cursor.execute(sql,(title, body , author))
+        db.commit()
+        
+        return redirect("/articles")
+    else:
+        return render_template('add_articles.html')
+
+    db.close()
+
+@app.route('/article/<string:id>/edit_article',methods=['GET', 'POST'])
+def edit_article(id):
+    if request.method =="POST":
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        cur = db.cursor()
+        sql = '''
+            UPDATE `topic` SET `title`=%s,`body`=%s, `author`=%s  WHERE  `id`= %s;
+        '''
+        cur.execute(sql , (title,body,author, id ))
+        db.commit()
+        return redirect(url_for('articles'))
+    else:
+        print(id)
+        cur = db.cursor()
+        sql = 'SELECT * FROM topic WHERE id=%s'
+        cur.execute(sql , [id])
+        topic = cur.fetchone()
+        return render_template('edit_article.html', data= topic)
+    db.close()
+
+@app.route('/delete/<string:id>',methods = ['POST'])
+def delete(id):
+   cursur = db.cursor()
+   sql = 'DELETE FROM `topic` WHERE  `id`=%s;'
+   cursur.execute(sql,[id])
+   db.commit()
+   return redirect(url_for('articles'))
+
 
 if __name__ =='__main__':
     # app.run(host='0.0.0.0', port='8080')
